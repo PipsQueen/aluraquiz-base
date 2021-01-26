8a213e7b1f784715d161aff95015baf06b030e3d
PipsQueen <ivangfdias@gmail.com>
2021-01-26 20:33:59 -0300

    Aula 02

--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,5 +1,4 @@
-import styled from "styled-components";
-import db from "../../../db.json";
+import styled from 'styled-components';
 
 export const Widget = styled.div`
   margin-top: 24px;
@@ -47,6 +46,55 @@ Widget.Content = styled.div`
   ul {
     list-style: none;
     padding: 0;
+  }
+`;
+
+// Widget.Form = styled.a`
+//   padding: 24px 32px 32px 32px;
+//   display: inline-flex;
+//   box-align: center;
+//   margin-left: 24px;
+//   background-color: ${({ theme }) => theme.colors.primary};
+//   opacity: 1;
+//   //&:hover : opacity:1;
+// `;
+
+Widget.Input = styled.input`
+  font-family: 'Lato', sans-serif;
+  display: flex;
+  padding: 16px 16px 16px 16px;
+  width: 100%;
+  border-radius: 4px;
+  border: 3px solid ${({ theme }) => theme.colors.primary};
+  opacity: 0.8;
+  background: ${({ theme }) => theme.colors.mainBg};
+  color: ${({ theme }) => theme.colors.contrastText};
+  margin: none;
+  &::placeholder{
+    color: ${({ theme }) => theme.colors.contrastText};
+    opacity: 0.9;
+  }
+  &:focus{
+    outline:none;
+  }
+`;
+
+Widget.Button = styled.button`
+  font-family: 'Lato', sans-serif;
+  transition-duration: 0.3s;  
+  background-color: ${({ theme }) => theme.colors.primary};
+  border-radius: 4px;
+  border: none;
+  margin-top: 4px;
+  padding: 16px 16px 16px 16px;
+  text-align: center;
+  display:flex;
+  color: ${({ theme }) => theme.colors.contrastText};
+  width:100%;
+  cursor: pointer;
+  &:disabled{
+    background-color: ${({ theme }) => theme.colors.secondary};
+    cursor: not-allowed;
   }
 `;
 
